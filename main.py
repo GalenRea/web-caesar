b283e4af82d994f6543fe539c658640e7bce83a8
Alyssa Rea <alyssa_joy@Alyssa-Reas-MacBook-Pro.local>
2018-03-30 18:09:41 -0500

    made changes

--- main.py
+++ main.py
@@ -1,55 +1,55 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from caesar import rotate_string
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-@app.route('/', methods=['POST'])
-def encrypt():
-    rot = int(request.form['rot'])
-    text = int(request.form['text'])
-    new_encryption = rotate_string(text, rot)
-    return form.format("""<h1>new_encryption</h1>""")
+form="""<!DOCTYPE html>
 
-
-form = """
-<!DOCTYPE html>
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
+            }}
             textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-                {0}
             }}
         </style>
     </head>
     <body>
-      <form method="POST">
-        <label>
-            I want to encrypt the message:
-            <input type="text" name="text"/>
-            to the degree of:
-            <input type="text" name="rot"/>
-            
-        </label>
-        <input type="submit" value="Encrypt It"/>
-    </form>
+        <form method="POST">
+            Rotate By : <input value="0" name="rot">
+			<textarea name="text" rows="4" cols="50">{0}</textarea>
+            <input value="Submit Query" type="submit">
+        </form>
     </body>
-</html> 
-"""
+</html>"""
+
 @app.route("/")
 def index():
     return form.format("")
+   
 
+
+@app.route('/', methods=['POST'])
+def encrypt():
+    rot = int(request.form['rot'])
+    text = request.form['text']
+    new_enc = rotate_string(text,rot)
+    
+    return form.format(new_enc)
+    
 
 app.run()
+
+
+
+
